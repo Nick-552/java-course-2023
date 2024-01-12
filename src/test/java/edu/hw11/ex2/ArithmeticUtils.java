91a4e4cf7ef2bcc3fca9070f144e5ea64851476c
Nick-552 <115263760+Nick-552@users.noreply.github.com>
2024-01-12 12:32:17 +0300

    Hw 11 (#18)
    * hw 11 done
    
    * fixed test failure (class redefinition failed: attempted to delete a method
    )

---
+++ src/test/java/edu/hw11/ex2/ArithmeticUtils.java
@@ -0,0 +1,11 @@
+package edu.hw11.ex2;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ArithmeticUtils {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
